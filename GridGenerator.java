977ea4902de029171469d5ba6fd744fe35edc177
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2020-04-15 14:42:51 +0300

    Updates Visualization code with for better windows positioning

--- GridGenerator.java
+++ GridGenerator.java
@@ -10,26 +10,32 @@ import java.util.Random;
 import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Component;
 import java.util.ArrayList;
 import java.awt.Canvas;
 
 class GridGenerator{
-	public static void VisualizeGrid(String frame_name, int N, int M, int [] walls, int [] grass, int start_idx, int terminal_idx ){
+	public static void VisualizeGrid(String frame_name, int xPos, int yPos, int N, int M, int [] walls, int [] grass, int start_idx, int terminal_idx ){
 		JFrame frame = new JFrame(frame_name);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Canvas canvas = new Drawing(N,M,walls,grass,start_idx,terminal_idx);
 		canvas.setSize(M*30,N*30);
 		frame.add(canvas);
 		frame.pack();
+		Dimension size = frame.getSize();
+		frame.setLocation((int)(size.getWidth()*xPos), (int)(size.getHeight()*yPos));
 		frame.setVisible(true);
 	}
-	public static void VisualizeGrid(String frame_name, int N, int M, int [] walls, int [] grass, int [] steps ,int start_idx, int terminal_idx ){
+	public static void VisualizeGrid(String frame_name, int xPos, int yPos, int N, int M, int [] walls, int [] grass, int [] steps ,int start_idx, int terminal_idx ){
 		JFrame frame = new JFrame(frame_name);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Canvas canvas = new Drawing(N,M,walls,grass, steps, start_idx,terminal_idx);
 		canvas.setSize(M*30,N*30);
 		frame.add(canvas);
 		frame.pack();
+		Dimension size = frame.getSize();
+		frame.setLocation((int)(size.getWidth()*xPos), (int)(size.getHeight()*yPos));
 		frame.setVisible(true);
 	}
 
@@ -49,25 +55,31 @@ class GridGenerator{
 		}
 		int N = mygrid.getNumOfRows();
 		int M = mygrid.getNumOfColumns();
-		//VisualizeGrid(frame,N,M,mygrid.getWalls(),mygrid.getGrass(),mygrid.getStartidx(),mygrid.getTerminalidx());
+		VisualizeGrid(frame,0,0,N,M,mygrid.getWalls(),mygrid.getGrass(),mygrid.getStartidx(),mygrid.getTerminalidx());
 		
 		//Breadth First Search 
 		BFS bfs = new BFS();
 		State goalStateBFS = bfs.breadthFirstSearch(mygrid);
 		System.out.println("\n\n--------Breadth First Search---------\n\n");
-		VisualizeGrid(frame,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateBFS),mygrid.getStartidx(),mygrid.getTerminalidx());
+		VisualizeGrid("BFS",1,0,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateBFS),mygrid.getStartidx(),mygrid.getTerminalidx());
 		
 		//Depth First Search
 		DFS dfs = new DFS();
 		State goalStateDFS = dfs.depthFirstSearch(mygrid);
 		System.out.println("\n\n--------Depth Fisrt Search---------\n\n");
-		VisualizeGrid(frame,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateDFS),mygrid.getStartidx(),mygrid.getTerminalidx());
+		VisualizeGrid("DFS",2,0,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateDFS),mygrid.getStartidx(),mygrid.getTerminalidx());
 		
 		//AStar Search
 		AStar astar = new AStar();
 		State goalStateAStar = astar.aStarSearch(mygrid);
 		System.out.println("\n\n--------ASTAR---------\n\n");
-		VisualizeGrid(frame,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateAStar),mygrid.getStartidx(),mygrid.getTerminalidx());
+		VisualizeGrid("AStar",3,0,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateAStar),mygrid.getStartidx(),mygrid.getTerminalidx());
+
+		//LRTAStar Search
+		// LRTAStar lartastar = new LRTAStar();
+		// State goalStateLRTAStar = lartastar.LRTAStarSearch(mygrid);
+		// System.out.println("\n\n--------LRTAStar---------\n\n");
+		// VisualizeGrid("LRTAStar",0,1,N,M,mygrid.getWalls(),mygrid.getGrass(),generateSteps(goalStateLRTAStar),mygrid.getStartidx(),mygrid.getTerminalidx());
 	}
 
 	public static int[] generateSteps(State state){
