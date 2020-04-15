40de3eecabb6731fa69bf90d9917eef3f8223871
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2020-04-15 15:14:01 +0300

    Moves partA project into subfolder for organization

--- project1/partA/LRTAStar.java
+++ project1/partA/LRTAStar.java
@@ -7,29 +7,33 @@ public class LRTAStar {
         State s = null;
         State a = null;
 
-        Random rand = new Random();
         HashMap<Cell, Double> H = new HashMap<Cell, Double>();
+        HashMap<Cell, State> S = new HashMap<Cell, State>();
         State sPrime = new State(grid, grid.getStart());
 
         int count = 1;
         while (true) {
             if (sPrime.goalReached()) {
-                return sPrime.getParentNode();
+                return S.get(sPrime.getParentNode().getCell());
             }
             else {
                 if (!H.containsKey(sPrime.getCell())) {
                     H.put(sPrime.getCell(), sPrime.findH());
+                    S.put(sPrime.getCell(), sPrime);
                 }
                 
                 if (s != null) {
                     double min = Double.MAX_VALUE;
+                    State sMin = null;
                     for (State state : s.successorStates(false)) {
                         double cost = LRTACost(s, state, H);
                         if (cost < min) {
                             min = cost;
+                            sMin = state;
                         }
                     }
                     H.put(s.getCell(), min);
+                    S.put(s.getCell(), sMin);
                 }
 
                 double min = Double.MAX_VALUE;
