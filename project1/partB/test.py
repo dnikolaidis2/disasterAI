2ecac44e0adbb1b96d4f9b485022b83df9d7eb3b
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2020-04-27 18:08:31 +0300

    Small fixes

--- project1/partB/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import crossover_function as cross
-
-A = np.zeros((30, 14))
-B = np.ones((30, 14))
-C = np.array([A, B])
-#print(C[0][0:3])
-print(cross.crossover(C, 'TwoPoint'))
