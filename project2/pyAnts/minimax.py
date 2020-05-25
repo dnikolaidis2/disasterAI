c92dc7fc1feaaccc8eec500252a1e80abc3ea7c1
Stavros Ntaountakis <daoudos@hotmail.gr>
2020-05-25 22:04:50 +0300

    Added a-b switch

--- project2/pyAnts/minimax.py
+++ project2/pyAnts/minimax.py
@@ -1,5 +1,5 @@
 
-def minimax(node, depth, maximizingPlayer, a, b):
+def minimax(node, depth, maximizingPlayer, a, b, abswitch):
     # What makes a node Terminal?
     if depth == 0 or node.is_terminal():
         return node.state_evaluation()
@@ -8,9 +8,9 @@ def minimax(node, depth, maximizingPlayer, a, b):
         value = -100000
 
         for child in node.successor_states():
-            value = max(value, minimax(child, depth-1, False, a, b))
+            value = max(value, minimax(child, depth-1, False, a, b, abswitch))
             a = max(value, a)
-            if a >= b:
+            if a >= b and abswitch:
                 break 
 
         return value
@@ -19,9 +19,9 @@ def minimax(node, depth, maximizingPlayer, a, b):
         value = +100000
 
         for child in node.successor_states():
-            value = min(value, minimax(child, depth-1, True, a, b))
+            value = min(value, minimax(child, depth-1, True, a, b, abswitch))
             b = min(b, value)
-            if a >= b:
+            if a >= b and abswitch:
                 break 
         
         return value
